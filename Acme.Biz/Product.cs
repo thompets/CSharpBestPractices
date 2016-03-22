52c91a8873b2031d602f81de0f5a94f6d4a67261
Pete Thompson <peter.thompson55@gmail.com>
2016-03-22 13:41:29 -0400

    Finished Module 4

--- Acme.Biz/Product.cs
+++ Acme.Biz/Product.cs
@@ -1,15 +1,19 @@
+using Acme.Common;
+using static Acme.Common.LoggingService;
 using System;
 
-namespace AcmeBiz
+namespace Acme.Biz
 {
 	/// <summary>
 	/// Manages products carried in inventory.
 	/// </summary>
 	public class Product
     {
+		#region Constructors
 		public Product()
 		{
 			Console.WriteLine("Product instance created");
+			//this.ProductVendor = new Vendor();
 		}
 
 		public Product(int productId, string productName, string description) : this()
@@ -20,33 +24,63 @@ namespace AcmeBiz
 
 			Console.WriteLine("Product instance has a name: " + ProductName);
 		}
+		#endregion
 
+		#region Properties
 		private string productName;
 		private string description;
 		private int productId;
+		private Vendor productVendor;
 
 		public string ProductName
 		{
 			get { return productName; }
 			set { productName = value; }
-		}		
+		}
 
 		public string Description
 		{
 			get { return description; }
 			set { description = value; }
-		}		
+		}
 
 		public int ProductId
 		{
 			get { return productId; }
 			set { productId = value; }
+		}		
+
+		public Vendor ProductVendor
+		{
+			get
+			{
+				// Lazy loading approach
+				if (productVendor == null)
+				{
+					productVendor = new Vendor();
+				}
+
+				return productVendor;
+			}
+			set { productVendor = value; }
 		}
 
+		#endregion
+
+		#region Methods
 		public string SayHello()
 		{
-			return "Hello " + ProductName + 
+			//var vendor = new Vendor();
+			//vendor.SendWelcomeEmail("Message from Product");
+
+			var emailService = new EmailService();
+			var confirmation = emailService.SendMessage("New Product", this.ProductName, "sales@abc.com");
+
+			var result = LogAction("saying hello");
+
+			return "Hello " + ProductName +
 				" (" + ProductId + "): " + Description;
-		}
+		} 
+		#endregion
 	}
 }
--- Tests/Acme.BizTests/ProductTests.cs
+++ Tests/Acme.BizTests/ProductTests.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AcmeBiz.Tests
+namespace Acme.Biz.Tests
 {
 	[TestClass()]
 	public class ProductTests
@@ -9,12 +9,11 @@ namespace AcmeBiz.Tests
 		public void SayHelloTest()
 		{
 			// Arrange - set up test
-			var currentProduct = new Product
-			{
-				ProductName = "Saw",
-				ProductId = 1,
-				Description = "15-inch steel blade hand saw"
-			};
+			var currentProduct = new Product();
+			currentProduct.ProductName = "Saw";
+			currentProduct.ProductId = 1;
+			currentProduct.Description = "15-inch steel blade hand saw";
+			currentProduct.ProductVendor.CompanyName = "ABC Corp";			
 
 			var expected = "Hello Saw (1): 15-inch steel blade hand saw";
 
@@ -35,6 +34,42 @@ namespace AcmeBiz.Tests
 
 			// Act
 			var actual = currentProduct.SayHello();
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void SayHello_ObjectInitializer()
+		{
+			// Arrange
+			var currentProduct = new Product
+			{
+				ProductId = 1,
+				ProductName = "Saw",
+				Description = "15-inch steel blade hand saw"
+			};
+
+			var expected = "Hello Saw (1): 15-inch steel blade hand saw";
+
+			// Act
+			var actual = currentProduct.SayHello();
+
+			// Assert
+			Assert.AreEqual(expected, actual);
+		}
+
+		[TestMethod]
+		public void Product_Null()
+		{
+			// Arrange
+			Product currentProdcut = null;
+			var companyName = currentProdcut?.ProductVendor?.CompanyName;
+
+			string expected = null;
+
+			// Act
+			var actual = companyName;
 
 			// Assert
 			Assert.AreEqual(expected, actual);
